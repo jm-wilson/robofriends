5d1c41cea0d9ce05d338ad4922a7fa9ac4e8f6d3
jm-wilson <joe.wilson@jwils.xyz>
2021-07-12 22:44:52 -0400

    CardList returns empty div if robots is empty

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,7 +2,9 @@ import React from 'react';
 import Card from './Card.js';
 
 const CardList = ({ robots }) => {
-  return (
+  return !robots.length ? (
+    <div></div>
+  ) : (
     <div>
       {robots.map((user, i) => {
         return <Card key={i} id={robots[i].id} name={robots[i].name} email={robots[i].email} />;
